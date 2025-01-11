9a2aa04d0fb94f2813dc604430477df1f2c817e0
Ansel <>
2025-01-11 11:49:47 +0300

    backend

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("SERVER STARTING")
+import axios from 'axios'
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+
+app.use(cors())
+app.use(express.static('dist'))
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
